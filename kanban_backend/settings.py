63c71f2baef397441b3c05434713a96ca3a28112
dhruvindsd-dev <dhruvinddev@gmail.com>
2022-06-29 12:18:13 +0530

    - replaced postgressql with sqllite

--- kanban_backend/settings.py
+++ kanban_backend/settings.py
@@ -96,17 +96,13 @@ WSGI_APPLICATION = "kanban_backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": env("DATABASE_NAME"),
-        "USER": env("DATABASE_USER"),
-        "PASSWORD": env("DATABASE_PASSWORD"),
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
